ab914eff868fb1a4eee5d08763252a53765a22e2
SantiSaaUCB <santiago.saavedra@ucb.edu.bo>
2025-10-25 12:19:13 -0400

    Imports

--- app/src/main/java/com/bearkicks/app/features/profile/presentation/ProfileScreen.kt
+++ app/src/main/java/com/bearkicks/app/features/profile/presentation/ProfileScreen.kt
@@ -1,6 +1,5 @@
 package com.bearkicks.app.features.profile.presentation
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
